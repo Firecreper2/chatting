584ce2b5bce8b06d8d5071ac5a6f7a8f975a08db
dylan <64336537+dylann123@users.noreply.github.com>
2022-11-14 08:49:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -35,6 +35,6 @@ app.get("/", function (req, res) {
 //Error handling
 app.use(function (req, res) { return res.status(404).send("404 page not found"); });
 //Listen!
-server.listen(80, function () {
+server.listen(process.env.PORT, function () {
     console.log("Poggers (it up)");
 });
